b814aec5631bbd42e0807663f8f7d8e5d5acb70d
docnexus <drlamb1@gmail.com>
2020-05-06 22:59:48 -0500

    moved to mongo backend

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require('lodash');
+
+mongoose.connect("mongodb://localhost:27017/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
+const blogSchema = new mongoose.Schema({
+  postTitle: String,
+  postContent: String
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
 
 const app = express();
 
@@ -19,10 +30,12 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+
+  Blog.find({}, function(err, foundBlogs) {
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      blogs: foundBlogs
+    });
   });
 });
 
@@ -48,31 +61,37 @@ app.get("/compose", function(req, res) {
 });
 
 app.get("/posts/:postId", function(req, res) {
-  const myId = _.lowerCase(req.params.postId);
-  let found = "false";
-  if (posts.length > 0) {
-    posts.forEach(function(post) {
-      const storedTitle = _.lowerCase(post.postTitle);
-      if (myId === storedTitle) {
+  const myId = req.params.postId;
+  console.log(myId);
+  Blog.findOne({_id: myId}, function(err, results) {
+    if (!err) {
+      if (!results) {
+        res.redirect('/');
+      } else {
+        console.log(results);
         res.render("post", {
-          post: post
+          title: results.postTitle,
+          content: results.postContent
         });
-        found = "true";
       }
-    });
-  }
-  if (found === "false") {
-    res.redirect("/");
-  }
+    }
+  });
 });
 
 app.post('/addContent', function(req, res) {
-  const post = {
-    postTitle: req.body.postTitle,
+
+  const newBlogPostTitle = _.capitalize(req.body.postTitle);
+
+  const newBlog = new Blog({
+    postTitle: newBlogPostTitle,
     postContent: req.body.postContent
-  }
-  posts.push(post);
-  res.redirect('/');
+  });
+  newBlog.save(function(err) {
+    if (!err) {
+      res.redirect('/');
+    }
+  });
+
 });
 
 
